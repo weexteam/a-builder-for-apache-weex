77649b3d0c0c48594e823ba71d5dd45cdce673a9
erha19 <faterrole@gmail.com>
2017-09-27 11:36:33 +0800

    feat:Support directory hot compile automatically

--- bin/weex-builder.js
+++ bin/weex-builder.js
@@ -6,11 +6,12 @@ const program = require('commander')
 const builder = require('../builder.js')
 const chalk = require('chalk')
 const Gauge = require('gauge')
+const pathTool = require('path')
 let showHelp = true
 program.version(require('../package.json').version)
     .option('-v,--version', 'show version')
     .option('-e,--ext [ext]', 'set enabled extname for compiler default is vue|we')
-    .option('-w,--web', 'set web mode for h5 render')
+    .option('-web,--web', 'set web mode for h5 render')
     .option('-w,--watch', 'watch files and rebuild')
     .option('-d,--devtool [devtool]', 'set webpack devtool mode')
     .option('-m,--min', 'compress the output js (will disable inline-source-map)')
@@ -20,7 +21,6 @@ program.version(require('../package.json').version)
         let gauge = new Gauge()
         let maxProgress = 0
         let babelConfig
-        // if (program.config)
         builder.build(source, dest, {
             onProgress: function (complete, action) {
                 if (complete > maxProgress) {
@@ -33,20 +33,20 @@ program.version(require('../package.json').version)
             },
             watch: program.watch,
             devtool: program.devtool,
-            ext: program.ext || '',
+            ext: pathTool.extname(source) || program.ext || 'vue',
             web: !!program.web,
             min: !!program.min,
         }, function (err, output, json) {
             gauge.hide()
             if (err) {
-                console.log(chalk.red('build failed!'))
+                console.log(chalk.red('Build Failed!'))
                 console.error(chalk.red(err))
                 if (err.stack)console.error(err.stack)
             }
             else {
-                console.log(chalk.green('build completed!\noutput:'))
-                console.log(chalk.green(output.toString()))
-                console.log(chalk.green('\nTime: ' + json.time + 'ms'))
+                console.log('Build completed!\nChild')
+                console.log(output.toString())
+                console.log(`Time: ${chalk.bold(json.time)}ms`)
             }
 
         })
--- builder/WeexBuilder.js
+++ builder/WeexBuilder.js
@@ -7,85 +7,92 @@ const pathTool = require('path')
 const webpack = require('webpack')
 const BannerPlugin = require('../plugin/WebpackBannerPlugin')
 const ext2framework = {
-    '.vue': 'Vue',
-    '.we': 'Weex',
-    '.js': 'Vue'
+  '.vue': 'Vue',
+  '.we': 'Weex',
+  '.js': 'Vue'
 }
 let defaultExt = ['we', 'vue', 'js']
-
 class WeexBuilder extends WebpackBuilder {
-    constructor(source, dest, options = {}) {
-        if (options.ext && typeof options.ext === 'string') {
-            options.ext = options.ext.split(/,|\|/).filter(e=>defaultExt.indexOf(e) === -1).concat(defaultExt).join('|')
-        }
-        else {
-            options.ext = defaultExt.join('|')
-
-        }
-        super(source, dest, options)
+  constructor(source, dest, options = {}) {
+    if (options.ext && typeof options.ext === 'string') {
+      options.ext = options.ext.split(/,|\|/).filter(e => defaultExt.indexOf(e) === -1).concat(defaultExt).join('|')
+    } else {
+      options.ext = defaultExt.join('|')
     }
-    loadModulePath (moduleName, extra) {
-        try {
-            let path = require.resolve(pathTool.join(moduleName, extra || ''))
-            return path.slice(0, path.lastIndexOf(moduleName) + moduleName.length)
-        } catch (e) {
-            return moduleName
-        }
+    super(source, dest, options)
+  }
+  loadModulePath(moduleName, extra) {
+    try {
+      let path = require.resolve(pathTool.join(moduleName, extra || ''))
+      return path.slice(0, path.lastIndexOf(moduleName) + moduleName.length)
+    } catch (e) {
+      return moduleName
     }
-    initConfig () {
-        super.initConfig()
-        let weexLoader = this.loadModulePath('weex-loader')
-        let vueLoader = this.loadModulePath('vue-loader')
-        let bannerPlugin = new BannerPlugin(function (file) {
-            let ext = pathTool.extname(file)
-            return '// { "framework": "' + ext2framework[ext] + '" }'
-        })
+  }
+  initConfig() {
+    super.initConfig()
+    let weexLoader = this.loadModulePath('weex-loader')
+    let vueLoader = this.loadModulePath('vue-loader')
+    let bannerPlugin = new BannerPlugin(function (file) {
+      let ext = pathTool.extname(file)
+      return '// { "framework": "' + ext2framework[ext] + '" }'
+    })
 
-        this.config.entry = this.source.map(s=>s + '?entry=true')
+    // webpack entry config
+    this.config.entry = this.source.map(s => s + '?entry=true')
+    
+    // webpack watch config
+    this.config.watch = this.options.watch || false
 
-        this.config.module.loaders.push({
-            test: /\.js(\?[^?]+)?$/,
-            loader: this.loadModulePath('babel-loader'),
-            exclude: /node_modules/
-        })
-        this.config.module.loaders.push({
-            test: /\.we(\?[^?]+)?$/,
-            loader: weexLoader
-        })
-        this.config.babel = {
-            presets: [this.loadModulePath('babel-preset-es2015')],
-            plugins: [
-                this.loadModulePath('babel-plugin-transform-runtime'),
-                this.loadModulePath('babel-plugin-add-module-exports')
-            ],
-            babelrc:true
-        }
-        if (this.options.web) {
-            this.config.module.loaders.push({
-                test: /\.vue(\?[^?]+)?$/,
-                loader: vueLoader
-            })
-        }
-        else {
-            this.config.module.loaders.push({
-                test: /\.vue(\?[^?]+)?$/,
-                loader: weexLoader
-            })
-        }
-        this.config.resolveLoader = {
-            root: pathTool.dirname(weexLoader)
-        }
-        this.config.resolve = {
-            alias: {
-                'babel-runtime': this.loadModulePath('babel-runtime', 'core-js'),
-                'babel-polyfill': this.loadModulePath('babel-polyfill'),
-            }
-        }
-        this.config.plugins.push(bannerPlugin)
-        if (this.options.onProgress) {
-            this.config.plugins.push(new webpack.ProgressPlugin(this.options.onProgress))
-        }
+    // webpack devtool config
+    this.config.devtool = this.options.devtool || ''
+
+    // webpack module config
+    this.config.module.loaders.push({
+      test: /\.js(\?[^?]+)?$/,
+      loader: this.loadModulePath('babel-loader'),
+      exclude: /node_modules/
+    })
+    this.config.module.loaders.push({
+      test: /\.we(\?[^?]+)?$/,
+      loader: weexLoader
+    })
+    if (this.options.web) {
+      this.config.module.loaders.push({
+        test: /\.vue(\?[^?]+)?$/,
+        loader: vueLoader
+      })
+    } else {
+      this.config.module.loaders.push({
+        test: /\.vue(\?[^?]+)?$/,
+        loader: weexLoader
+      })
     }
+
+    // webpack resolve config
+    this.config.resolveLoader = {
+      root: pathTool.dirname(weexLoader)
+    }
+    this.config.resolve = {
+      alias: {
+        'babel-runtime': this.loadModulePath('babel-runtime', 'core-js'),
+        'babel-polyfill': this.loadModulePath('babel-polyfill'),
+      }
+    }
+
+    // webpack plugins config
+    if (this.options.min) {
+      this.config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      }))
+    }
+    this.config.plugins.push(bannerPlugin)
+    if (this.options.onProgress) {
+      this.config.plugins.push(new webpack.ProgressPlugin(this.options.onProgress))
+    }
+
+  }
 }
 module.exports = WeexBuilder
-
